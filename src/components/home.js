762f7a01937df3c8b4a6a2ba8864eaf8143fcb2b
Leo Lu <lyuholu@gmail.com>
2025-12-02 22:00:21 -0500

    fix: add wafer image

--- src/components/home.js
+++ src/components/home.js
@@ -1,17 +1,21 @@
-import Carousel from "../components/carousel";
+import Carousel from '../components/carousel';
 
 export default function Home() {
-  const slides = [
-    { id: 1, src: "/images/chips.jpg"},
-    { id: 2, src: "/images/chocolate.jpg"},
-    { id: 3, src: "/images/cookie.jpg"},
-    { id: 4, src: "/images/popcorn.jpg"},
-    { id: 5, src: "/images/pretzel.jpg"},
-  ];
+    const slides = [
+        { id: 1, src: '/images/chips.jpg' },
+        {
+            id: 2,
+            src: 'https://samwangbucket.s3.us-east-1.amazonaws.com/wafer.png',
+        },
+        { id: 3, src: '/images/chocolate.jpg' },
+        { id: 4, src: '/images/cookie.jpg' },
+        { id: 5, src: '/images/popcorn.jpg' },
+        { id: 6, src: '/images/pretzel.jpg' },
+    ];
 
-  return (
-    <section className="home">
-      <Carousel slides={slides} />
-    </section>
-  );
+    return (
+        <section className="home">
+            <Carousel slides={slides} />
+        </section>
+    );
 }
